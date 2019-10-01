9ec8d1ec28ddf69bcbc439a5ca49ab026e7caaaa
Tim Elgersma <timothy.elgersma@gmail.com>
2019-09-30 21:34:37 -0400

    Some code cleanup

--- content.js
+++ content.js
@@ -1,89 +1,93 @@
 function handleText(textNode, courses) {
-    /*
+  /*
     Can't add a child to a <p> so instead create a span containing
     the text and the new link.
     */
-    if (textNode.nodeName !== '#text' ||
-        textNode.parentNode.nodeName === 'SCRIPT' ||
-        textNode.parentNode.nodeName === 'STYLE') {
-        return;
-    }
-    let originalText = textNode.textContent;
-    let newHTML = originalText
+  if (
+    textNode.nodeName !== "#text" ||
+    textNode.parentNode.nodeName === "SCRIPT" ||
+    textNode.parentNode.nodeName === "STYLE"
+  ) {
+    return;
+  }
+  let originalText = textNode.textContent;
+  let newHTML = originalText;
 
-    //let newHTML = originalText
-    courses.forEach(function (coursePair) {
-        newHTML = replaceCourse(coursePair.code, coursePair.name, newHTML)
-    })
+  courses.forEach(coursePair => {
+    newHTML = replaceCourse(coursePair.code, coursePair.name, newHTML);
+  });
 
-    if (newHTML !== originalText) {
-        let newSpan = document.createElement('span');
-        newSpan.innerHTML = newHTML;
-        textNode.parentNode.replaceChild(newSpan, textNode);
-    }
+  if (newHTML !== originalText) {
+    let newSpan = document.createElement("span");
+    newSpan.innerHTML = newHTML;
+    textNode.parentNode.replaceChild(newSpan, textNode);
+  }
 }
 
 function replaceCourse(courseCode, courseName, text) {
-    var regexCourseCode = new RegExp(courseCode, "gi");
-    return text.replace(
-        regexCourseCode,
-        "<a href='' title='" + courseName +
-        "' style='background-color:white;color:black;text-decoration:none'>" + courseCode + "</a>"
-    )
+  let regexCourseCode = new RegExp(courseCode, "gi");
+  return text.replace(
+    regexCourseCode,
+    `<a href='' title='${courseName}' style='background-color:white;color:black;text-decoration:none'> ${courseCode} </a>`
+  );
 }
 
 function linkCourses(subject, nodeList, insertCourses) {
-    let courses = []
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://www.ucalendar.uwaterloo.ca/1819/COURSE/course-" + subject + ".html", true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            var coursePage = $('<div></div>');
-            coursePage.html(xhr.responseText);
-            codePattern = RegExp(subject + "\\s*\\d\\d\\d[A-Z]?", "gi") // SUBJ [arbitrary spacing] [3 digits] [char modifier]
-            let tables = $('table', coursePage)
-            for (i = 4, l = tables.length; i < l; i++) {
-                codePattern.lastIndex = 0; // reset regex position
+  let courses = [];
+  let xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    `https://www.ucalendar.uwaterloo.ca/1819/COURSE/course-${subject}.html`,
+    true
+  );
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4) {
+      let coursePage = $("<div></div>");
+      coursePage.html(xhr.responseText);
+      codePattern = RegExp(subject + "\\s*\\d\\d\\d[A-Z]?", "gi"); // SUBJ [arbitrary spacing] [3 digits] [char modifier]
+      let tables = $("table", coursePage);
+      for (i = 4, l = tables.length; i < l; i++) {
+        codePattern.lastIndex = 0; // reset regex position
 
-                courseInfo = $('table', coursePage)[i].rows;
+        courseInfo = $("table", coursePage)[i].rows;
 
-                if (courseInfo.length === 0) {
-                    break;
-                }
-
-                let code = codePattern.exec(courseInfo[0].textContent)[0]
-                let name = courseInfo[1].textContent
-                courses.push({ code, name })
-            }
-            insertCourses(nodeList, courses)
+        if (courseInfo.length === 0) {
+          break;
         }
+
+        let code = codePattern.exec(courseInfo[0].textContent)[0];
+        let name = courseInfo[1].textContent;
+        courses.push({ code, name });
+      }
+      insertCourses(nodeList, courses);
     }
-    xhr.send()
+  };
+  xhr.send();
 }
 
 let traverser = document.createTreeWalker(
-    document.body,
-    NodeFilter.SHOW_TEXT,
-    {
-        acceptNode: function (node) {
-            if (node.textContent.length === 0) {
-                return NodeFilter.FILTER_SKIP; // skip empty text
-            }
-            return NodeFilter.FILTER_ACCEPT;
-        }
-    },
-    false
+  document.body,
+  NodeFilter.SHOW_TEXT,
+  {
+    acceptNode: node => {
+      if (node.textContent.length === 0) {
+        return NodeFilter.FILTER_SKIP; // skip empty text
+      }
+      return NodeFilter.FILTER_ACCEPT;
+    }
+  },
+  false
 );
 
-let nodeList = []
+let nodeList = [];
 while (traverser.nextNode()) {
-    nodeList.push(traverser.currentNode);
+  nodeList.push(traverser.currentNode);
 }
 
 function traverseNodes(nodeList, courseList) {
-    nodeList.forEach(function (node) {
-        handleText(node, courseList);
-    })
+  nodeList.forEach(node => {
+    handleText(node, courseList);
+  });
 }
 
 let coursesCS = linkCourses("CS", nodeList, traverseNodes);
